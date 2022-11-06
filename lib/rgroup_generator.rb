8255fef41f3b514d5eb38155c4c95c828ec300e4
An Nguyen <caman.nguyenthanh@gmail.com>
2022-11-06 13:05:54 +0000

    Handle bond 8 and 10

--- lib/rgroup_generator.rb
+++ lib/rgroup_generator.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
-require 'debug'
-
 # {RgroupGenerator} generates molecules from rgroup information
 class RgroupGenerator
+  BOND_TYPE_HYDROGEN = 14
+  public_constant :BOND_TYPE_HYDROGEN
+
+  BOND_TYPE_OTHER = 20
+  public_constant :BOND_TYPE_OTHER
+
   # Constructor
   def initialize(mdl:, rgroup_data:)
     @mdl = mdl
@@ -22,8 +26,9 @@ class RgroupGenerator
         MoleculeExpander.new(rw_mol: rw_mol, rgroup: rgroup, superatom: superatom).call
       end
 
-      mdl = rw_mol.mol_to_mol_block(true, -1, false)
-      inchi = Inchi.molfileToInchi(mdl, Inchi::ExtraInchiReturnValues.new, '-Polymers')
+      mdl = rw_mol.mol_to_mol_block(true, -1, false, true)
+      inchi = try_get_inchi(mdl)
+
       molecule = data.merge(
         mdl: mdl,
         smiles: rw_mol.mol_to_smiles(true),
@@ -41,5 +46,38 @@ class RgroupGenerator
     RDKitChem::RWMol.mol_from_mol_block(mdl)
   rescue RuntimeError
     RDKitChem::RWMol.mol_from_mol_block(mdl, false)
+  end
+
+  # Try to get inchi from mdl
+  def try_get_inchi(mdl)
+    inchi = Inchi.molfileToInchi(mdl, Inchi::ExtraInchiReturnValues.new, '-Polymers')
+    return inchi unless inchi.empty?
+
+    mol = rdkit_mol_from_mdl(mdl)
+    bonds_to_remove = get_zero_order_bonds(mol)
+
+    bonds_to_remove.each do |bond|
+      mol.remove_bond(bond[0], bond[1])
+    end
+
+    new_mdl = mol.mol_to_mol_block(true, -1, true, true)
+    Inchi.molfileToInchi(new_mdl, Inchi::ExtraInchiReturnValues.new, '-Polymers')
+  end
+
+  # Get zero-order bonds
+  def get_zero_order_bonds(mol)
+    zero_order_bonds = []
+    (0..(mol.get_num_bonds - 1)).map do |idx|
+      bond = mol.get_bond_with_idx(idx)
+
+      case bond.get_bond_type
+      when BOND_TYPE_HYDROGEN
+        zero_order_bonds.push([bond.get_begin_atom_idx, bond.get_end_atom_idx])
+      when BOND_TYPE_OTHER
+        bond.set_bond_type(1)
+      end
+    end
+
+    zero_order_bonds
   end
 end
